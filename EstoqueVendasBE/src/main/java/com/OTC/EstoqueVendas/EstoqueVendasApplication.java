bbb428e321aeab29c5e5c89b1becc8e6cd4b80a6
Th4llesSo4res <thalles.gtrafego@gmail.com>
2025-05-28 20:14:44 -0300

    primerio commit da aplicação

---
+++ EstoqueVendasBE/src/main/java/com/OTC/EstoqueVendas/EstoqueVendasApplication.java
@@ -0,0 +1,13 @@
+package com.OTC.EstoqueVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueVendasApplication.class, args);
+	}
+
+}
---
+++ EstoqueVendasBE/src/test/java/com/OTC/EstoqueVendas/EstoqueVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OTC.EstoqueVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
